f619153e02b10eb6164d5f6bb502ead5654c110e
Andy Hui <andy.hui@oocl.com>
2025-09-05 15:48:49 +0800

    Test: Add test case for moving rover when its facing West

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -52,4 +52,20 @@ class DemoTest {
         assertEquals(expectedLocation.getYCoordinate(), marsRover.getLocation().getYCoordinate());
         assertEquals(expectedLocation.getDirection(), marsRover.getLocation().getDirection());
     }
+
+    @Test
+    void should_move_upward_when_executeCommand_given_Command_M_And_Direction_W() {
+        //Given
+        Location location = new Location(0,0, Direction.W);
+        MarsRover marsRover = new MarsRover(location);
+
+        // When
+        marsRover.executeCommand(Command.M);
+
+        // Then
+        Location expectedLocation = new Location(-1,0, Direction.W);
+        assertEquals(expectedLocation.getXCoordinate(), marsRover.getLocation().getXCoordinate());
+        assertEquals(expectedLocation.getYCoordinate(), marsRover.getLocation().getYCoordinate());
+        assertEquals(expectedLocation.getDirection(), marsRover.getLocation().getDirection());
+    }
 }
